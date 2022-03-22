6095fec9acb8ab28d9230396fb16ca809b51f9c0
Brando <jose.brando.c.m@gmail.com>
2022-03-22 15:08:10 -0600

    [DEV] Ubicación en tiempo real

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  latitud: number;
+  longitud: number;
+
   constructor() {}
+
+  async obtenerCoordenadas() {
+    const obtenerCoordenada = await Geolocation.getCurrentPosition();
+  
+    this.latitud = obtenerCoordenada.coords.latitude;
+    this.longitud = obtenerCoordenada.coords.longitude;
+  }
 
 }
